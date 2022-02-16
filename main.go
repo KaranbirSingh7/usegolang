f7a022759bdf79fa135c814a58f41eedeae6e0a9
Karanbir Singh <karanbir_singh@manulife.com>
2022-02-16 08:38:51 -0500

    add: main

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func galleryHandler(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "userID")
+	fmt.Fprintf(w, "Passed value: %v", id)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "Welcome to website")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "Contact US")
+}
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "FAQs Page")
+}
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.Get("/gallery/{userID}", galleryHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
+	fmt.Println("Server starting on :3000")
+	http.ListenAndServe(":3000", r)
+}
+
+bad code here
