358a4cc0a1e5a22b66eed6601672d551b286d573
Karanbir Singh <karanbir_singh@manulife.com>
2022-02-28 16:26:50 -0500

    home page with template

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"path/filepath"
 
@@ -20,11 +21,15 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	tplPath := filepath.Join("templates", "home.gohtml")
 	tpl, err := template.ParseFiles(tplPath)
 	if err != nil {
-		panic(err)
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing template.", http.StatusInternalServerError)
+		return
 	}
 	err = tpl.Execute(w, nil)
 	if err != nil {
-		panic(err)
+		log.Printf("executing temple: %v", err)
+		http.Error(w, "There was an error executing the template", http.StatusInternalServerError)
+		return
 	}
 }
 
