9e2347f862c54e10a6761b6870c6610830156739
Karanbir Singh <karanbir_singh@manulife.com>
2022-03-03 17:57:44 -0500

    add: views package

--- main.go
+++ main.go
@@ -2,30 +2,22 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"log"
 	"net/http"
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/karanbirsingh7/usegolang/views"
 )
 
-// function to execute templates in Handler
 func executeTemplate(w http.ResponseWriter, filepath string) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tpl, err := template.ParseFiles(filepath)
+	t, err := views.Parse(filepath)
 	if err != nil {
-		log.Printf("parsing template: %v", err)
-		http.Error(w, "There was an error parsing templat", http.StatusInternalServerError)
+		http.Error(w, "There was an error parsing template.", http.StatusInternalServerError)
 		return
 	}
-	err = tpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("executing template: %v", err)
-		http.Error(w, "There was an error executing template", http.StatusInternalServerError)
-		return
-	}
+
+	t.Execute(w, nil)
 }
 
 func galleryHandler(w http.ResponseWriter, r *http.Request) {
