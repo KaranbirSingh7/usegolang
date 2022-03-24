ac8996ffb49fc7b3846f866a11254651d6eee2eb
karanbirsingh7 <karanbirsingh1161@gmail.com>
2022-03-23 21:51:37 -0400

    email query + UX autocomplete

--- controllers/users.go
+++ controllers/users.go
@@ -12,30 +12,16 @@ type Users struct {
 }
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Email string
+	}
+	data.Email = r.FormValue("email")
 	// users object will have a template already parsed that can be rendered
-	u.Templates.New.Execute(w, nil)
+	u.Templates.New.Execute(w, data)
 }
 
 // Create creates new user sent by signUp form
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
-	// err := r.ParseForm()
-	// if err != nil {
-	// 	http.Error(w, err.Error(), http.StatusBadRequest)
-	// 	return
-	// }
-
-	// email := r.PostForm.Get("email")
-	// password := r.PostForm.Get("password")
-
-	// // validate
-	// if len(email) == 0 {
-	// 	http.Error(w, "email cannot be empty", http.StatusBadRequest)
-	// 	return
-	// }
-	// if len(password) == 0 {
-	// 	http.Error(w, "password cannot be empty", http.StatusBadRequest)
-	// 	return
-	// }
 	email := r.FormValue("email")
 	password := r.FormValue("password")
 	fmt.Fprintf(w, email, password)
